ef26f127ce66b9df7945225169b6fc74ec7b5af2
Joy Mozumder <joy.mozumder.1003@gmail.com>
2022-11-23 13:44:09 +0600

    full package cdn

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-md-7 offset-3 mt-4">
+            <div class="col-md-12 mt-4">
                 <div class="card-body">
                     <form action="" method="post" enctype="multipart/form-data">
                         @csrf
@@ -23,8 +23,7 @@
             </div>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-    <script src="//cdn.ckeditor.com/4.14.1/standard/ckeditor.js"></script>
+    <script src="//cdn.ckeditor.com/4.20.0/full/ckeditor.js"></script>
     <script>
         <script type="text/javascript">
             $(document).ready(function () {
